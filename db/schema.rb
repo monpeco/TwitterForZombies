d6bd8758c79521eeee58873d31ccbf2db01f2068
monpeco <monpeco@gmail.com>
2017-01-31 13:41:58 +0000

    Run db migrate after g scaffold tweet

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124184009) do
+ActiveRecord::Schema.define(version: 20170131133637) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "status"
+    t.string   "body"
+    t.integer  "zombie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "zombies", force: :cascade do |t|
     t.string   "name"
     t.text     "bio"
-    t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.boolean  "rotting"
   end
 
 end
