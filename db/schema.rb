8df146e71b4156bf607414d773f4e868a60d9ced
monpeco <monpeco@gmail.com>
2017-02-25 23:29:31 +0000

    Run db migrate to delete age from zombie

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131133637) do
+ActiveRecord::Schema.define(version: 20170208022741) do
+
+  create_table "brains", force: :cascade do |t|
+    t.integer  "zombie_id"
+    t.string   "status"
+    t.string   "flavor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "status"
