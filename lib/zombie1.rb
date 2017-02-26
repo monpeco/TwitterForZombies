f1c05a566641234cdf7839bafddce27c5967802c
monpeco <monpeco@gmail.com>
2017-02-26 04:56:32 +0000

    Add crud operation (tested on rails console)

--- lib/zombie1.rb
+++ lib/zombie1.rb
@@ -3,3 +3,26 @@ puts b[:id]
 puts b[:zombie]
 puts b[:zombie] + " said: " + b[:status]
 
+#Create
+t = Tweet.new
+t.status = "I love brains"
+t.zombie_id = 1
+t.save
+
+t = Tweet.new
+t.status = "McDonalds is so nice"
+t.zombie_id = 1
+t.save
+
+#Read
+t = Tweet.find(2)
+
+#Update
+t = Tweet.find(2)
+t.status = "McDonalds is so terrific"
+t.save
+
+#Delete
+t = Tweet.find(2)
+t.destroy
+
