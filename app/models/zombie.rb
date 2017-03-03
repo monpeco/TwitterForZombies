0c4b71a9d53fd0c9a9b11dc3d2cdc98e1fc27f30
monpeco <monpeco@gmail.com>
2017-03-03 01:28:47 +0000

    Update relationship between zombie and tweet

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -2,5 +2,7 @@ class Zombie < ActiveRecord::Base
     validates :name, presence: true
     validates :rotting, inclusion: { in: [true], message: 'no puede estar en false o nil' }
     validates :bio, exclusion: { in: %w(www us ca jp), message: "%{value} is reserved." }
+    
+    has_many :tweets
 end
 
