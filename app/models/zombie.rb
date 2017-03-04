2b0bf0a843f937b554f7d8f2beb0cbe6391c3884
monpeco <monpeco@gmail.com>
2017-03-04 21:14:10 +0000

    Add validation both minimum and maximum in model

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,5 +1,6 @@
 class Zombie < ActiveRecord::Base
-    validates :name, presence: true, length: { minimum: 2 }
+    validates :name, presence: true, 
+    length: {minimum: 2, maximum: 5}
     validates :rotting, inclusion: { in: [true], message: 'no puede estar en false o nil' }
     validates :bio, exclusion: { in: %w(www us ca jp), message: "%{value} is reserved." }
     
