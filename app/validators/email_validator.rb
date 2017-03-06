4d4b02d4e3b0a1744ae8d18e378386cd10fe9fce
monpeco <monpeco@gmail.com>
2017-03-06 14:06:28 +0000

    Fix custom email validator, validates presence and format

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -4,9 +4,9 @@ class EmailValidator < ActiveModel::Validator
       value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
    end
    def validate(record)
-      if record.present? && record =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
+      if record.email.present? && record.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
          return true 
       end
-      puts record[options[:email]]
+      record.errors[:base] << "provide a proper email"
    end
 end
