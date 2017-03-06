bd4083a3141fbe66d23e661a3ae17a33cd556c6c
monpeco <monpeco@gmail.com>
2017-03-06 14:12:19 +0000

    Remove validate each method (never used)

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,8 +1,4 @@
 class EmailValidator < ActiveModel::Validator
-   def validate_each(record, attribute, value)
-      record.errors.add attribute, (option[:message] || "it is not a email") unless 
-      value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-   end
    def validate(record)
       if record.email.present? && record.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
          return true 
