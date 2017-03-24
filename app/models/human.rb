cd1f2ee15efb78618a68e098b8a4c80caa717c61
monpeco <monpeco@gmail.com>
2017-03-24 12:46:14 +0000

    Add how to degub human model (Rails.logger.debug)

--- app/models/human.rb
+++ app/models/human.rb
@@ -2,6 +2,9 @@ class Human < ActiveRecord::Base
     scope :yongers, -> { where(age: 0..30) }    #lazy evaluated when it is called
     scope :with_power, -> (power) { where(power: power) }
     
+    @current_user = 44
+    Rails.logger.debug "@current_user,: #{@current_user}"
+        
     def human_base(attribute)
         Human.find(attribute)
     end
