cce60b4fa2d7673d63a185bf600a0f5180d05a90
monpeco <monpeco@gmail.com>
2017-03-26 16:49:04 +0000

    Add article resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'welcome#index'    #tells Rails to map requests to the root of the application to the welcome controller's index action
   
   get 'welcome/index'     #tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+
+  resources :articles     #create a new resource 'articles'
   
   # root "humen#index" # get "/", "zombies#index" 
 
