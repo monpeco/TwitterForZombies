5cbb762aaaf7e9ba0433d63057a957dd59bf7c36
monpeco <monpeco@gmail.com>
2017-04-04 00:34:19 +0000

    Nokogiri working, recover title from page

--- lib/scrapping/web_scraper.rb
+++ lib/scrapping/web_scraper.rb
@@ -14,11 +14,26 @@ parse_page = Nokogiri::HTML(page)
 
 pets_array = []
 
+#<title>new york pets  - craigslist</title>
+puts parse_page.css("title")[0].text
+
+#<a href="/mnh/pet/6072743772.html" data-id="6072743772" class="result-title hdrlnk">Loving Cat in need of New Home</a>
+#links = parse_page.css('a')
+puts parse_page.css("div").map do |a|
+	post_name = a.text
+	pets_array.push(post_name)
+end
+
+
+#puts parse_page.css("title")[0].text
+
 # this is where we parse the data
+"""
 parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
 	post_name = a.text
 	pets_array.push(post_name)
 end
+"""
 
 # this will push your array into a CSV file
 CSV.open('pets.csv', 'w') do |csv|
