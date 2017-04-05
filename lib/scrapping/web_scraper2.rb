c219293c02b45b52538d96732bec2d2eeeff2940
monpeco <monpeco@gmail.com>
2017-04-05 16:54:42 +0000

    Add write file g-class and comment other functions

--- lib/scrapping/web_scraper2.rb
+++ lib/scrapping/web_scraper2.rb
@@ -3,19 +3,27 @@ require 'nokogiri'
 require 'csv'
 
 doc = Nokogiri::HTML(open("http://www.google.com/search?q=doughnuts"))
-doc.xpath('//h3/a').each do |node|
-  puts node.text
-end
+#  puts doc
+#Print <div class="g">
+  doc.xpath('//div[@class = "g"]').each do |node|
+    puts node.text
+  end
+  
+File.open('doughnuts.txt', 'w') { |file| file.write(doc.xpath('//div[@class = "g"]')) }
 
-puts '-----------------'
+# doc.xpath('//h3/a').each do |node|
+#   puts node.text
+# end
 
-item_array = []
-doc.xpath('//h3[@class = "r"]/a').each do |node|
-  puts node.text
-  item_array.push(node.text)
-end
+# puts '-----------------'
+
+# item_array = []
+# doc.xpath('//h3[@class = "r"]/a').each do |node|
+#   puts node.text
+#   item_array.push(node.text)
+# end
 
 # this will push your array into a CSV file
-CSV.open('pets.csv', 'w') do |csv|
-	csv << item_array
-end
+# CSV.open('pets.csv', 'w') do |csv|
+# 	csv << item_array
+# end
