096ac043787a6991b377b60001682b173484b280
monpeco <monpeco@gmail.com>
2017-04-12 21:25:57 +0000

    Create a date spliting (into date and hour)

--- public/doc/dates/week.rb
+++ public/doc/dates/week.rb
@@ -1,4 +1,10 @@
 require 'date'
 
-date = Date.today
-puts date.to_time.strftime('%b %d')
+datetime = "2017-04-12 10:30:14"
+
+my_date = DateTime.parse(datetime).strftime("%d/%m/%Y")
+my_hour = DateTime.parse(datetime).strftime("%H:%M")
+
+puts my_date
+puts my_hour
+
