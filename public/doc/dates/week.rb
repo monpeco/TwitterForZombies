80dd571ae74e7d7f23125488980f2a2929fb7b58
monpeco <monpeco@gmail.com>
2017-04-12 21:27:01 +0000

    Add another date spliting (into date and hour) with to_time method

--- public/doc/dates/week.rb
+++ public/doc/dates/week.rb
@@ -8,3 +8,14 @@ my_hour = DateTime.parse(datetime).strftime("%H:%M")
 puts my_date
 puts my_hour
 
+puts "--------"
+
+require 'active_support/core_ext/string'
+
+datetime2 = "11/04/2017 23:00".to_time
+
+my_date2 = datetime2.strftime("%d/%m/%Y")
+my_hour2 = datetime2.strftime("%H:%M")
+
+puts my_date2
+puts my_hour2
