f4bb37b3c6e3510621320d0652ff18fe213a072d
monpeco <monpeco@gmail.com>
2017-04-30 23:58:49 +0000

    Create a helper method to use cookies

--- app/helpers/zombies_helper.rb
+++ app/helpers/zombies_helper.rb
@@ -1,2 +1,16 @@
 module ZombiesHelper
+
+    #The bad way
+    def add_to_cookie (id)
+        unless cookies[:scotids]
+            cookies[:scotsids] = JSON.generate(["#{id}"])
+        else
+            cookies[:scotsids] = JSON.parse(cookies[:scotsids]) << "#{id}"
+        end
+    end
+
+    #The not so bad way
+    def add_to_cookie_2 (id)
+        cookies[:scotids] ? cookies[:scotsids] = JSON.parse(cookies[:scotsids]) << "#{id}" : cookies[:scotsids] = JSON.generate(["#{id}"])
+    end
 end
