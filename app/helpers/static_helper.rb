c2929280782f873b0ee0f91a50f6262e172ab53d
monpeco <monpeco@gmail.com>
2017-05-17 13:24:08 +0000

    Create ageing helper (in static_helper) and use it

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,15 @@
 module StaticHelper
+
+    def ageing age
+        case age
+          when -Float::INFINITY..20
+            'You are too young'
+          when 21..64
+            'You are at the right age'
+          when 65..Float::INFINITY
+            'You are too old'
+        end
+    end
+    
+    
 end
