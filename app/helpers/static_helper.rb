55257eb8337186d9a103aaea1e13297c37989d46
monpeco <monpeco@gmail.com>
2017-05-18 18:00:25 +0000

    Add fibonacci Hash definition (fast-memoization)

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -12,5 +12,14 @@ module StaticHelper
         end
     end
     
+
+
+  def fibo num
+    fibonacci = Hash.new{ |numbers,index|
+      numbers[index] = fibonacci[index - 2] + fibonacci[index - 1]
+    }.update(0 => 0, 1 => 1)
+    
+    fibonacci[num]
+  end
     
 end
